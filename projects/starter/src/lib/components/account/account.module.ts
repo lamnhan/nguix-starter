f944ca92f3a1614c4448cadf4fbd3fb74b9be898
Lam Nhan <lamnhan92@gmail.com>
2021-07-26 16:59:03 +0700

    update account comp

--- projects/starter/src/lib/components/account/account.module.ts
+++ projects/starter/src/lib/components/account/account.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@ngneat/transloco';
+import { RouterLinkDirectiveModule } from '@lamnhan/ngx-useful';
+
+import { NguixIconComponentModule } from '../icon/icon.module';
 
 import { AccountComponent } from './account.component';
 
@@ -9,6 +12,8 @@ import { AccountComponent } from './account.component';
   imports: [
     CommonModule,
     TranslocoModule,
+    RouterLinkDirectiveModule,
+    NguixIconComponentModule,
   ],
   exports: [AccountComponent]
 })
