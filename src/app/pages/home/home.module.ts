ad9b1026a5dd3440d19a9a227137728e8b53a717
Lam Nhan <lamnhan92@gmail.com>
2021-09-23 22:31:25 +0700

    fix posts and skeleton posts

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,9 +6,9 @@ import { NguixIconComponentModule, NguixContentComponentModule } from '@lamnhan/
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-import { PostsDataPipeModule } from '@lamnhan/ngx-schemata';
-import { NguixPostsComponentModule } from '../../../../projects/starter/src/lib/components/posts/posts.module';
-import { NguixSkeletonPostsComponentModule } from '../../../../projects/starter/src/lib/components/skeleton-posts/skeleton-posts.module';
+import { VideosDataPipeModule } from '@lamnhan/ngx-schemata';
+import { NguixVideosComponentModule } from '../../../../projects/starter/src/lib/components/videos/videos.module';
+import { NguixSkeletonVideosComponentModule } from '../../../../projects/starter/src/lib/components/skeleton-videos/skeleton-videos.module';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,9 @@ import { NguixSkeletonPostsComponentModule } from '../../../../projects/starter/
     NguixIconComponentModule,
     NguixContentComponentModule,
     HomeRoutingModule,
-    PostsDataPipeModule,
-    NguixPostsComponentModule,
-    NguixSkeletonPostsComponentModule,
+    VideosDataPipeModule,
+    NguixVideosComponentModule,
+    NguixSkeletonVideosComponentModule,
   ]
 })
 export class HomePageModule {}
