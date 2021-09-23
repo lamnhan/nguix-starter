744a607fdfb722a5eb87dc7638ffee7560fa8d7d
Lam Nhan <lamnhan92@gmail.com>
2021-09-23 21:28:03 +0700

    update posts thumbnail layout

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,9 +7,9 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 
-import { PostsDataPipeModule } from '@lamnhan/ngx-schemata';
-import { NguixPostsComponentModule } from '../../../../projects/starter/src/lib/components/posts/posts.module';
-import { NguixSkeletonPostsComponentModule } from '../../../../projects/starter/src/lib/components/skeleton-posts/skeleton-posts.module';
+import { VideosDataPipeModule } from '@lamnhan/ngx-schemata';
+import { NguixVideosComponentModule } from '../../../../projects/starter/src/lib/components/videos/videos.module';
+import { NguixSkeletonVideosComponentModule } from '../../../../projects/starter/src/lib/components/skeleton-videos/skeleton-videos.module';
 
 @NgModule({
   declarations: [
@@ -21,9 +21,9 @@ import { NguixSkeletonPostsComponentModule } from '../../../../projects/starter/
     NguixIconComponentModule,
     NguixContentComponentModule,
     HomeRoutingModule,
-    PostsDataPipeModule,
-    NguixPostsComponentModule,
-    NguixSkeletonPostsComponentModule,
+    VideosDataPipeModule,
+    NguixVideosComponentModule,
+    NguixSkeletonVideosComponentModule,
   ]
 })
 export class HomePageModule {}
